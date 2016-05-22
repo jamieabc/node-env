0d4b933ab3db0a9ba916482988d33b1f37bbfe6a
Aaron Chang <aaron.chang@vpon.com>
2016-05-22 12:29:53 +0800

    add react basic component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>'hihi'</p>
+      </div>
+    );
+  }
+}
+
+export default App;
